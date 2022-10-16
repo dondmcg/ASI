a8bb1af439dadcb69ecf1215b5ace0946d96634a
dondmcg <don@mcgrathdesigns.com>
2022-10-16 19:50:34 -0400

    first commit to ASI

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,25 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
-import FeedbackForm from "@components/FeedbackForm";
-import JokeBlock from "@components/JokeBlock";
+import {Button} from "@mui/material";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>ASI</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox" />
+        <Header title="Welcome to ASI" />
         <hr />
         <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
+          This page is yet to be designed so stand by to get your socks blown off!!
         </p>
-        <FeedbackForm />
-        <JokeBlock />
+        <Button style={{maxWidth: '240px', maxHeight: '30px', minWidth: '240px', minHeight: '30px'}} variant="contained" onClick={() => {
+          alert('clicked');
+        }}>Add your wallet</Button>
       </main>
       <Footer />
     </div>
